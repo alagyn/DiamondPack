c62fa3b5ba1f563e3ebb6dfc2db619520993baca
alagyn <benkimbrough44@gmail.com>
2023-12-18 16:18:29 -0500

    fix var names

--- diamondpack/app-templates/app.sh
+++ diamondpack/app-templates/app.sh
@@ -3,7 +3,7 @@
 home=$(realpath $(dirname $0))
 PY_VER=$(ls $home/venv/lib | grep python)
 
-export PYTHON_HOME=${home}/venv/stdlib
-export PYTHON_PATH=${home}/venv/lib/$PY_VER/site-packages
+export PYTHONHOME=${home}/venv/stdlib
+export PYTHONPATH=${home}/venv/lib/$PY_VER/site-packages
 export LD_LIBRARY_PATH=${home}/venv/bin
 ${home}/venv/bin/python -m @@SCRIPT
